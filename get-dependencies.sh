77954b266741c7758e5242c5b907f1d8a247736c
juju4 <juju4@users.noreply.github.com>
2017-01-03 09:08:31 -0500

    missing symlink when using Jenkinsfile

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,4 +12,6 @@ fi
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
+## galaxy naming
+[ ! -e $rolesdir/juju4.mhn ] && ln -s ansible-mhn $rolesdir/juju4.mhn
 
