575a8a9a4cacc9fdd0b954384e0384def7e88a5f
juju4 <juju4@users.noreply.github.com>
2017-01-03 03:29:41 -0500

    collector disabled in defaults settings

--- test/integration/default/serverspec/mhncollector_spec.rb
+++ test/integration/default/serverspec/mhncollector_spec.rb
@@ -3,8 +3,9 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/var/log/mhn/mhn-collector.log') do
-  its(:content) { should match /Connecting to / }
-  its(:content) { should match /Connected to / }
-  its(:content) { should_not match /Error/ }
-end
+## default, collector is disabled
+#describe file('/var/log/mhn/mhn-collector.log') do
+#  its(:content) { should match /Connecting to / }
+#  its(:content) { should match /Connected to / }
+#  its(:content) { should_not match /Error/ }
+#end
