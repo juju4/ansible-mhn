dde9e77bcc6a5742dc981253e39015482b31d253
juju4 <juju4@users.noreply.github.com>
2017-01-04 10:39:50 -0500

    fix test in Jenkinsfile+kitchen

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,10 @@ fi
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
-## galaxy naming
-[ ! -e $rolesdir/juju4.mhn ] && ln -s ansible-mhn $rolesdir/juju4.mhn
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.mhn ] && ln -s ansible-mhn $rolesdir/juju4.mhn
+[ ! -e $rolesdir/juju4.mhn ] && cp -R $rolesdir/ansible-mhn $rolesdir/juju4.mhn
+
+## don't stop build on this script return code
+true
 
